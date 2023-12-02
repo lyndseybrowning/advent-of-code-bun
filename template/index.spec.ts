a5995097266dc701477ef6690ade37815665f91b
Lyndsey Browning <lbrowning86@gmail.com>
2023-12-02 19:18:12 +0000

    chore: create template

---
+++ template/index.spec.ts
@@ -0,0 +1,11 @@
+import { expect, test, describe } from 'bun:test'
+
+import { getParsedInput, part1, part2 } from '.'
+
+describe('day{day}', () => {
+  test.skip('parsed input', () => {})
+
+  test('part 1', () => {})
+
+  test('part 2', () => {})
+})
---
+++ template/index.ts
@@ -0,0 +1,15 @@
+export const part1 = () => {}
+
+export const part2 = () => {}
+
+export const getParsedInput = (input: string[]) => {}
+
+const input = Bun.file('./2023/{day}/input.txt')
+
+const textInput = await input.text()
+const parsedInput = getParsedInput(textInput.split('\n'))
+
+const p1 = part1()
+const p2 = part2()
+
+console.log({ p1, p2 })
