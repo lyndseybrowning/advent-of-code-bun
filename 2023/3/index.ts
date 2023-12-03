77a73043bf73e52fc3263edee2b4afe004125774
Lyndsey Browning <lbrowning86@gmail.com>
2023-12-03 14:35:49 +0000

    chore: refactor day 3 part2 solution

--- 2023/3/index.ts
+++ 2023/3/index.ts
@@ -5,9 +5,10 @@ type PartNumber = {
   number: number
   coordinate: string
 }
-/* a part number is a number that is adjacent to any symbol other than '.'
+
+/* A part number is a number that is adjacent to any of the provided symbols
  * this takes the given schematic input and produces a list of valid part numbers
- * along with the co-ordinates of each matched symbol when specified
+ * and the coordinate of the matched symbol
  */
 export const getPartNumbers = (
   schematic: string[],
@@ -23,18 +24,12 @@ export const getPartNumbers = (
       const end = start + match[0].length
       const range = [...Array(end - start).keys()].map((i) => i + start)
 
-      const adjacentValues = range.reduce<Adjacent<string>[]>(
-        (values: Adjacent<string>[], index: number) => {
-          return [
-            ...values,
-            ...getAdjacentValues<string>(row, index, schematic),
-          ]
-        },
-        []
-      )
+      const adjacents = range.flatMap<Adjacent<string>>((column: number) => {
+        return getAdjacentValues<string>(row, column, schematic)
+      })
 
-      const partNumber = adjacentValues.find((adjacent) =>
-        validSymbols.includes(adjacent.value as string)
+      const partNumber = adjacents.find(({ value }) =>
+        validSymbols.includes(value)
       )
 
       if (partNumber) {
@@ -55,12 +50,19 @@ export const part1 = (input: string[]): number => {
   return getSumOfDigits(partNumbers)
 }
 
-export const part2 = (input: string[]) => {
-  const partNumbers = getPartNumbers(input)
+export const part2 = (input: string[]): number => {
+  // only find part numbers that are adjacent to '*'
+  const partNumbers = getPartNumbers(input, '*')
   const gears = []
 
   while (partNumbers.length > 0) {
     const [{ number, coordinate }] = partNumbers
+
+    /*
+     * A gear exists when two numbers are adjacent to a * symbol
+     * If another part number exists with this partNumber's coordinate
+     * then it must be a gear
+     */
     const gear = partNumbers
       .slice(1)
       .find((number) => number.coordinate === coordinate)
